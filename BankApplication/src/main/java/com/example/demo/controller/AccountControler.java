af249ab5fe3674d94b92a2b419a31ea81366c069
divya426 <katamdivyasree1@gmail.com>
2025-05-09 15:48:01 -0400

    withdraw amount by id, get All accounts, de;ete account are completed

--- BankApplication/src/main/java/com/example/demo/controller/AccountControler.java
+++ BankApplication/src/main/java/com/example/demo/controller/AccountControler.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,9 +36,30 @@ public class AccountControler {
 		return new ResponseEntity<AccountDto>(accountService.createAccount(accountDto), HttpStatus.CREATED);
 	}
 	
-	@GetMapping("/getAccount")
-	public ResponseEntity<AccountDto> getAccount(@RequestParam Long id){
+	@GetMapping("/getAccount/{id}")
+	public ResponseEntity<AccountDto> getAccount(@PathVariable Long id){
 		return new ResponseEntity<AccountDto>(accountService.getAccount(id), HttpStatus.OK);
 	}
+	
+	@PatchMapping("/deposit/{id}")
+	public ResponseEntity<AccountDto> depositAmount(@PathVariable Long id, @RequestBody Map<String, Double> request ){
+		return new ResponseEntity<AccountDto>(accountService.deposit(id, request.get("amount")), HttpStatus.OK);
+	}
 
+	@PatchMapping("/withdraw/{id}")
+	public ResponseEntity<AccountDto> withdrawAmount(@PathVariable Long id, @RequestBody Map<String, Double> request){
+		return new ResponseEntity<AccountDto>(accountService.withdraw(id, request.get("amount")), HttpStatus.OK);
+	}
+	
+	@GetMapping("/getAllAccounts")
+	public ResponseEntity<List<AccountDto>> getAllAccount(){
+		return new ResponseEntity<List<AccountDto>>(accountService.getAllAccount(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteAccount(@PathVariable Long id){
+		accountService.deleteAccount(id);
+		return new ResponseEntity<String>("Account is deleted successfully", HttpStatus.OK);
+	}
+	
 }
--- BankApplication/src/main/java/com/example/demo/service/IAccountService.java
+++ BankApplication/src/main/java/com/example/demo/service/IAccountService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import com.example.demo.dto.AccountDto;
 
 
@@ -8,6 +10,8 @@ public interface IAccountService {
 	public AccountDto createAccount(AccountDto account);
 	public AccountDto getAccount(Long id);
 	public AccountDto deposit(Long id, Double amount);
-	
+	public AccountDto withdraw(Long id, Double amount );
+	public List<AccountDto> getAllAccount();
+	public void deleteAccount(Long id);
 
 }
